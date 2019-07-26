143dfdcd3d456d678dc2aa76ce9b6ee89f31ae72
root <nerradchiang@gmail.com>
2019-07-26 14:20:03 +0800

    Updated python script of client image.

--- client/app/client.py
+++ client/app/client.py
@@ -4,12 +4,11 @@ import sys
 import iperf3
 from time import sleep
 
-def get_server_info() {
+def get_server_info():
 	server = {}
 	return server
-}
 
-def test_iperf3(server, protocol, print_info) {
+def test_iperf3(server, protocol, print_info):
 	client = iperf3.Client()
 	client.server_hostname = server['hostname']
 	client.protocol = protocol
@@ -17,13 +16,12 @@ def test_iperf3(server, protocol, print_info) {
 	if print_info:
 		print(test)
 	return test
-}
 
 if __name__ == '__main__':
 	server = get_server_info()
 	results = []
 
 	while True:
-		results.append(('tcp', test_iperf3(server, 'tcp', true))
-		results.append(('udp', test_iperf3(server, 'udp', true))
+		results.append(('tcp', test_iperf3(server, 'tcp', True)))
+		results.append(('udp', test_iperf3(server, 'udp', True)))
 		sleep(3)
